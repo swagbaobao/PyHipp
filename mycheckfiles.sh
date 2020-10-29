85edb83ff85d1e37318153d31a3a07fea09c12df
AWS ParallelCluster user <ec2-user@ip-10-0-6-195.ap-southeast-1.compute.internal>
2020-10-29 16:15:08 +0000

    add lab 7 files

---
+++ mycheckfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | xargs ls -hl
+
+find mountains -name "firings.mda" | wc -l
+
+
